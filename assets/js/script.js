2b5ff08580d6ccb6f180823df1b9f2e0e3298a10
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-15 22:16:14 -0500

    updated the weather data columns, udpated the js code to display data

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var APIKey = '712010c635163bedc765625963bdff72';
+var cityname = $('#searchtext').val();
+console.log(cityname);
+
+// Here we are building the URL we need to query the database
+
+var queryURL = 'api.openweathermap.org/data/2.5/forecast?q=' + cityname + ',us';
+
+$.ajax({
+	url    : queryURL,
+	method : 'GET'
+}).then(function(response) {
+	console.log(response);
+
+	// Transfer content to HTML
+	$('.city').html('<h1>' + response.name + ' Weather Details</h1>');
+	$('.wind').text('Wind Speed: ' + response.wind.speed);
+	$('.humidity').text('Humidity: ' + response.main.humidity);
+	$('.temperature').text('Temperature (F) ' + response.main.temp);
+	$('.feels').text('Feels Like: ' + response.main.feels_like);
+});
