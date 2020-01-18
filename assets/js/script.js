d40cb83aaf47571185ba009d155c5665cceb672e
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-17 23:01:33 -0500

    js code for main weather info works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
-var APIKey = '712010c635163bedc765625963bdff72';
-var cityname = $('#searchtext').val();
-console.log(cityname);
+// var cityname = $('#searchtext').val();
+// console.log(cityname);
 
 // Here we are building the URL we need to query the database
 
+var APIKey = '712010c635163bedc765625963bdff72';
 var queryURL =
-	'api.openweathermap.org/data/2.5/forecast?q=' +
-	cityname +
-	',us&mode=JSON&apikey=' +
+	'https://api.openweathermap.org/data/2.5/forecast?q=atlanta,us&mode=JSON&apikey=' +
 	APIKey;
-console.log(queryURL);
+var queryURL2 =
+	'https://api.openweathermap.org/data/2.5/weather?q=atlanta&mode=JSON&apikey=' +
+	APIKey;
 
 $.ajax({
-	url    : queryURL,
+	url    : queryURL2,
 	method : 'GET'
-}).then(function(response) {
-	console.log(response);
+}).then(function(weather) {
+	console.log(weather);
+	console.log(queryURL2);
 
 	// Transfer content to HTML
-	// $('.city').html('<h1>' + response.name + ' Weather Details</h1>');
-	// $('.wind').text('Wind Speed: ' + response.wind.speed);
-	// $('.humidity').text('Humidity: ' + response.main.humidity);
-	// $('.temperature').text('Temperature (F) ' + response.main.temp);
-	// $('.feels').text('Feels Like: ' + response.main.feels_like);
+	$('.city').html(weather.name + ' Weather Details');
+	$('.wind').text('Wind Speed: ' + weather.wind.speed);
+	$('.humidity').text('Humidity: ' + weather.main.humidity);
+	$('.temperature').text('Temperature (F) ' + weather.main.temp);
+	$('.feels').text('Feels Like: ' + weather.main.feels_like);
 });
+
+// $.ajax({
+// 	url    : queryURL,
+// 	method : 'GET'
+// }).then(function(forecast) {
+// 	console.log(queryURL);
+// 	console.log(forecast);
+
+// 	// Transfer content to HTML
+// 	$('.temperature1').text('Temperature (F) ' + weather.list.main.temp);
+// 	$('.humidity1').text('Humidity: ' + weather.list.main.humidity);
+// });
