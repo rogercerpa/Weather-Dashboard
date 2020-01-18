aae31846ea7001c4ec1906c375d546d403511abf
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-17 23:45:31 -0500

    updated the forecast for days2-5 for only temp and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,18 +35,18 @@ $.ajax({
 
 	// Transfer content to HTML
 	//day 1
-	$('.temperature1').text('Temperature (F) ' + forecast.list[0].main.temp);
-	$('.humidity1').text('Humidity: ' + forecast.list[0].main.humidity);
-	//day2
-	// $('.temperature1').text('Temperature (F) ' + forecast.list[1].main.temp);
-	// $('.humidity1').text('Humidity: ' + forecast.list[1].main.humidity);
-	// //day3
-	// $('.temperature1').text('Temperature (F) ' + forecast.list[0].main.temp);
-	// $('.humidity1').text('Humidity: ' + forecast.list[0].main.humidity);
-	// //day4
-	// $('.temperature1').text('Temperature (F) ' + forecast.list[0].main.temp);
-	// $('.humidity1').text('Humidity: ' + forecast.list[0].main.humidity);
-	// //day5
-	// $('.temperature1').text('Temperature (F) ' + forecast.list[0].main.temp);
-	// $('.humidity1').text('Humidity: ' + forecast.list[0].main.humidity);
+	$('.temperature1').text('Temperature (F) ' + forecast.list[3].main.temp);
+	$('.humidity1').text('Humidity: ' + forecast.list[3].main.humidity);
+	// day2
+	$('.temperature2').text('Temperature (F) ' + forecast.list[11].main.temp);
+	$('.humidity2').text('Humidity: ' + forecast.list[11].main.humidity);
+	//day3
+	$('.temperature3').text('Temperature (F) ' + forecast.list[19].main.temp);
+	$('.humidity3').text('Humidity: ' + forecast.list[19].main.humidity);
+	//day4
+	$('.temperature4').text('Temperature (F) ' + forecast.list[27].main.temp);
+	$('.humidity4').text('Humidity: ' + forecast.list[27].main.humidity);
+	//day5
+	$('.temperature5').text('Temperature (F) ' + forecast.list[35].main.temp);
+	$('.humidity5').text('Humidity: ' + forecast.list[35].main.humidity);
 });
