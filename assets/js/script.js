78459238e031705112162904d8f61d7add9a9cc3
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-01-18 20:24:47 -0500

    temperature is now reflecting on fahrenheit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $('#search-btn').on('click', function(event) {
 	event.preventDefault();
 
+	var currentdate = moment();
+
 	//user text input city name
 	var cityname = $('#searchtext').val().trim();
 	//API KEY
@@ -24,13 +26,16 @@ $('#search-btn').on('click', function(event) {
 	}).then(function(weather) {
 		console.log(weather);
 		console.log(queryURL2);
+		var temp = JSON.stringify(weather.main.temp);
 
 		// Transfer content to HTML
 		$('.city').html(weather.name + ' Weather Details');
 		$('.wind').text('Wind Speed: ' + weather.wind.speed);
 		$('.humidity').text('Humidity: ' + weather.main.humidity);
-		$('.temperature').text('Temperature (F) ' + weather.main.temp);
-		$('.feels').text('Feels Like: ' + weather.main.feels_like);
+		$('.temperature').text(
+			'Temperature ' + ((temp - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
+		// $('.feels').text('Feels Like: ' + weather.main.feels_like);
 	});
 
 	$.ajax({
@@ -40,26 +45,43 @@ $('#search-btn').on('click', function(event) {
 		console.log(queryURL);
 		console.log(forecast);
 
+		var temp1 = JSON.stringify(forecast.list[3].main.temp);
+		var temp2 = JSON.stringify(forecast.list[11].main.temp);
+		var temp3 = JSON.stringify(forecast.list[19].main.temp);
+		var temp4 = JSON.stringify(forecast.list[27].main.temp);
+		var temp5 = JSON.stringify(forecast.list[35].main.temp);
+
 		// Transfer content to HTML
-		//day 1
+
+		//day1
 		$('.image1').text(forecast.list[3].weather[0].icon);
-		$('.temperature1').text('Temp: ' + forecast.list[3].main.temp + ' °F');
+		$('.temperature1').text(
+			'Temp: ' + ((temp1 - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
 		$('.humidity1').text('Humidity: ' + forecast.list[3].main.humidity + '%');
-		// day2
+		//day2
 		$('.image2').text(forecast.list[11].weather[0].icon);
-		$('.temperature2').text('Temp: ' + forecast.list[11].main.temp + ' °F');
+		$('.temperature2').text(
+			'Temp: ' + ((temp2 - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
 		$('.humidity2').text('Humidity: ' + forecast.list[11].main.humidity + '%');
 		//day3
 		$('.image3').text(forecast.list[19].weather[0].icon);
-		$('.temperature3').text('Temp: ' + forecast.list[19].main.temp + ' °F');
+		$('.temperature3').text(
+			'Temp: ' + ((temp3 - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
 		$('.humidity3').text('Humidity: ' + forecast.list[19].main.humidity + '%');
 		//day4
 		$('.image4').text(forecast.list[27].weather[0].icon);
-		$('.temperature4').text('Temp: ' + forecast.list[27].main.temp + ' °F');
+		$('.temperature4').text(
+			'Temp: ' + ((temp4 - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
 		$('.humidity4').text('Humidity: ' + forecast.list[27].main.humidity + '%');
 		//day5
 		$('.image5').text(forecast.list[35].weather[0].icon);
-		$('.temperature5').text('Temp: ' + forecast.list[35].main.temp + ' °F');
+		$('.temperature5').text(
+			'Temp: ' + ((temp5 - 273.15) * 1.8 + 32).toFixed(1) + ' °F'
+		);
 		$('.humidity5').text('Humidity: ' + forecast.list[35].main.humidity + '%');
 	});
 
